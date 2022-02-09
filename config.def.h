352b9e3864258aeb21b0de0ce9b6c5e20b19d2ed
ki11errabbit <unlikelytitan@gmail.com>
2022-02-08 20:22:11 -0700

    Added another fulscreen patch, added altkey

--- config.def.h
+++ config.def.h
@@ -60,6 +60,7 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod4Mask
+#define ALTKEY Mod1Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -102,20 +103,20 @@ static Key keys[] = {
 	{ MODKEY|ControlMask,		XK_l,      shiftswaptags,  { .i = +1 }	},
 	{ MODKEY|ShiftMask,             XK_l,      shiftboth,      { .i = +1 }	},
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ MODKEY|ALTKEY,                XK_h,      incrgaps,       {.i = +1 } },
+	{ MODKEY|ALTKEY,                XK_l,      incrgaps,       {.i = -1 } },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_h,      incrogaps,      {.i = +1 } },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_l,      incrogaps,      {.i = -1 } },
+	{ MODKEY|ALTKEY|ControlMask,    XK_h,      incrigaps,      {.i = +1 } },
+	{ MODKEY|ALTKEY|ControlMask,    XK_l,      incrigaps,      {.i = -1 } },
+	{ MODKEY|ALTKEY,                XK_0,      togglegaps,     {0} },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
 	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
 	{ MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
+	{ MODKEY|ALTKEY,                XK_y,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|ALTKEY,                XK_o,      incrohgaps,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
@@ -124,6 +125,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ALTKEY|ShiftMask,      XK_f,      fullscreen,     {0} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
