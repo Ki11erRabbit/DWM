f2612878b5614fc1bedd71046f13d8120b140a3c
ki11errabbit <unlikelytitan@gmail.com>
2022-04-15 21:28:06 -0600

    Moved hotkeys to SXHKD

--- config.def.h
+++ config.def.h
@@ -16,7 +16,7 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "LigalexMono:size=9",/*"IBMPlexMono-Regular:size=9",*/ /*"JoyPixels*/"twemoji:pixelsize=9:antialias=true:autohint=true" };
+static const char *fonts[]          = { "LigalexMono:size=9","twemoji:pixelsize=9:antialias=true:autohint=true","Fira Code Medium:size=9", "fontawesome-regular:size=9","monospace:size=9","Font Awesome 6 Free Solid:size=9"/*"IBMPlexMono-Regular:pixelsize=9"*/ /*"JoyPixels*/};
 static const char dmenufont[]      = "LigalexMono:size=9";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
